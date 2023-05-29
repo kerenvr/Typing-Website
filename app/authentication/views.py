5d0bc8edc651f94ce64a94f44aab6d4835119079
allyniti <119133399+allyniti@users.noreply.github.com>
2023-05-29 11:14:37 -0500

    login, signup, css, db

--- app/authentication/views.py
+++ app/authentication/views.py
@@ -1,7 +1,32 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_required, login_user, logout_user
+from werkzeug.security import check_password_hash, generate_password_hash
 
-auth = Blueprint('auth', __name__)\
+from .. import db
+from .models import User
 
-@auth.route('/login')
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET'])
 def login():
+    if request.method == 'GET':
+        email = request.args.get('email')
+        password = request.args.get('password')
+        #return redirect(url_for('views.home'))
+
     return render_template("login.html")
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        data = request.form
+        print(data)
+
+    return render_template("sign-up.html")
+
+
--- app/home/views.py
+++ app/home/views.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
-@views.route('/home')
+@views.route('/')
 def home():
     return render_template("home.html")
